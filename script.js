0787a1f075c93b361aec9ee5cd549b49ca912b0b
Gustavo Miguel <miguelgustavo004@gmail.com>
2023-09-08 14:11:41 -0300

    DomJS

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function enviar(){
+
+    var num = document.getElementById('txtnum')
+    var res = document.getElementById('res')
+    var re = Number(num.value)
+
+
+        if(re > 100 || re < 0)
+    {
+
+            alert('Os dados apresentados não podem ser analisados. Tente novamente!')
+
+    } 
+        else if (num.value.length == 0)
+    {
+            alert('Insira um número!')
+    }
+        else
+    {
+
+            res.innerHTML = `Seu número é: ${re}`
+
+    }
+    
+    
+
+
+}
+
+
+
+
+
+const chk = document.getElementById('chk')
+
+chk.addEventListener('change' , () => {
+    document.body.classList.toggle('dark')
+})
+
